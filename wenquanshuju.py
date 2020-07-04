3516808e00a8271d4de9f5205a020d41a2e33427
durongze <durongze@qq.com>
2020-07-04 19:11:58 +0800

    Update wenquanshuju.py

--- wenquanshuju.py
+++ wenquanshuju.py
@@ -12,14 +12,22 @@ import re
 import random
 from pathlib import Path
 import execjs
+import html
+import re
+from bs4 import BeautifulSoup
+from os.path import basename, splitext
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 http = urllib3.PoolManager()
 agent_list = [
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv2.0.1) Gecko/20100101 Firefox/4.0.1",
-"Mozilla/5.0 (Windows NT 6.1; rv2.0.1) Gecko/20100101 Firefox/4.0.1",
-"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
-"Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv2.0.1) Gecko/20100101 Firefox/4.0.1",
+    "Mozilla/5.0 (Windows NT 6.1; rv2.0.1) Gecko/20100101 Firefox/4.0.1",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+    "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"
 ]
 
 
@@ -44,13 +52,85 @@ def download_img(img_url, file_name):
     f.close()
 
 
-'img.onload = function(){ let baseSrc = document.querySelector("#pagebox div"); console.log(baseSrc.getAttribute("src")); }'
+def call_js_func(js_func, data, key):
+    js_ctx = execjs.compile(js_func)  # 加载JS文件
+    print("还不能使用 js 访问 python中的元素")
+    return js_ctx.call('get_item_src')  # 调用js方法  第一个参数是JS的方法名，后面的data和key是js方法的参数
 
-def download_book(book_url, item_xpath, download_dir):
+
+def find_target_items(text):
+    p=re.compile(r'[<](.*?)[>]', re.S)
+    matches = re.findall(p, text)
+    images = []
+    for match in matches:
+        print (match)
+        if match.find('img') == 0:  # 包含html代码，img在字符串首位
+            soup = BeautifulSoup("<"+match+">")
+            img = soup.find_all('img')
+            src=img[0].get('src')
+            images.append(src)
+        #else:
+            # images.append(match)  # 不包含html代码
+    return images
+
+def element_click(browser, element):
+    try:
+        ActionChains(browser).double_click(element).perform()
+        WebDriverWait(browser, 8, 0.5, ignored_exceptions=EC).until(element.is_displayed())
+        print('success')
+    except Exception as e:
+        print('fail')
+
+
+def write_element(data, file_name):
+    f = open(file_name, 'wb')
+    f.write(data)
+    f.close()
+
+
+def get_page_link(driver, element):
+    items = []
+    element_click(driver, element)
+    element_html = element.get_attribute('innerHTML')
+    items.append(element_html)
+    images = find_target_items(element_html)
+    return images
+
+
+def write_page_data(img_src, file_name):
+    opener = urllib.request.build_opener()
+    opener.addheaders = user_agent
+    data = urllib.request.urlopen(img_src).read()
+    f = open(file_name, 'wb')
+    f.write(data)
+    f.close()
+
+
+def get_book_pages(book_url, item_xpath, js_func_str, download_dir):
     driver = webdriver.Chrome()
     driver.get(book_url)
+    element_list = driver.find_elements_by_xpath(item_xpath)
+    all_pages = []
+    all_items = []
+    for element in element_list:
+        element_click(driver, element)
+        element_html = element.get_attribute('innerHTML')
+        all_items.append(element_html)
+        images = find_target_items(element_html)
+        all_pages = all_pages + images
     i = 0
-    for element in driver.find_elements_by_xpath(item_xpath):
+    for page in all_pages:
+        file_name = gen_local_path(download_dir, i, 'png')
+        write_page_data(page, file_name)
+        i = i + 1
+    return all_pages
+
+def download_book(book_url, item_xpath, js_func_str, download_dir):
+    driver = webdriver.Chrome()
+    driver.get(book_url)
+    element_list = driver.find_elements_by_xpath(item_xpath)
+    i = 0
+    for element in element_list:
         i = i + 1
         if i < 1:
             continue
@@ -58,21 +138,36 @@ def download_book(book_url, item_xpath, download_dir):
         if Path(file_name).exists():
             print("exist :%s" % file_name)
             continue
-        print(element.get_attribute('innerHTML'))
-        img_src = element.get_attribute('src')
+        element_click(driver, element)
+        # element.screenshot(file_name)
+        element_html = element.get_attribute('innerHTML')
+        print("====== ========\n ", element_html)
+        images = find_target_items(element_html)
+        img_src = images[0]
+        if img_src is None:
+            print("error page idx :%s" % i)
+            continue
+        # img_src = element.get_attribute('src')
+        # img_src = call_js_func(js_func_str, element.get_attribute('innerHTML'), "src")
+        # img_src = driver.execute_script(js_func_str, element_html, "src")
         opener = urllib.request.build_opener()
         opener.addheaders = user_agent
         data = urllib.request.urlopen(img_src).read()
         f = open(file_name, 'wb')
         f.write(data)
         f.close()
-        driver.close()
+    driver.close()
 
 
-btn_xpath = '//*[@id="pagebox"]/div'
-base_url = 'https://wqbook.wqxuetang.com/read/pdf/8888888888'
+btn_xpath = '//*[@class="page-img-box"]'
+base_url = 'https://wqbook.wqxuetang.com/read/pdf/11111111111'
 download_dir = 'E:\\webdown\\van'
 
 if __name__ == "__main__":
     user_agent = random.choice(agent_list)
-    download_book(base_url, btn_xpath, download_dir)
+    js_func_str = 'function get_item_src(){  return console.log("data"); }'
+    js_str = 'arguments[0].getAttribute(arguments[1], 4)'
+    #download_book(base_url, btn_xpath, js_str, download_dir)
+    all_pages = get_book_pages(base_url, btn_xpath, js_str, download_dir)
+    print(all_pages)
+
